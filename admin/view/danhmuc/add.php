2dc6a8a2fe35495b351b582fa6d0d8fc46aec32c
vuongvu <vuongvu10899@gmail.com>
2024-03-14 16:05:39 +0700

    main

--- admin/view/danhmuc/add.php
+++
@@ -1,7 +0,0 @@
-<form method="POST" action="index.php?action=them_danhmuc">
-  <div class="mb-3">
-    <label for="" class="form-label">Tên danh mục</label>
-    <input type="text" class="form-control" id="" name="ten_dm">
-  </div>
-  <button type="submit" class="btn btn-primary" name="them_moi">Thêm mới</button>
-</form>
